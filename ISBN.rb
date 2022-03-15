86ce51a08fde70b0b7036cde1da89399c914b4b0
SamArsanios <samson.kibrom@hotmail.com>
2022-03-15 11:32:36 +0300

    Modify method to include return value

--- ISBN.rb
+++ ISBN.rb
@@ -4,29 +4,33 @@ def isbn13(str)
     else
         arr = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1]
     end
-
-		splited = str.split("")
-        converted = splited.map(&:to_i)
-        # p converted
+        #split string values into separate values and convert them to integers
+		split_values = str.split("")
+        converted = split_values.map(&:to_i)
         
+        #multiply isbn array with its respective array and sum their product
         result = converted.zip(arr).map{|x, y| x * y}
         sum = result.inject(0){|sum,x| sum + x }
-        p sum
 
+        #check for validity of isbn 10 and isbn13
         if str.length == 10
             if sum % 11 == 0
                 value = "Valid"
-                p converted.unshift(978)
+                converted.unshift(978)
                 converted[-1] += 1
                 p converted.join("")
+                p value
             else
                 value = "Invalid"
+                p value
             end
         elsif str.length == 13
             if sum % 10 == 0
                 value = "Valid"
+                p value
             else
                 value = "Invalid"
+                p value
             end
         end
         value
